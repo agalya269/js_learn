e5c02e49df01e4e791d06c2d1501bb49bcc2bf95
agalya <agalyaep.20cse@kongu.edu>
2022-11-10 22:26:18 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,104 @@
+//console.log('hello');
+
+
+//let const
+/* let age=25;
+age = 30;                              output=30
+console.log(age);*/
+
+
+/* const age = 25;
+console.log(age);                      output=25*/
+
+
+/* const age=25;
+age = 30;                               output=error
+console.log(age);*/
+
+
+//data types
+/*const name="Agalya";
+const age = 20;
+const rating = 4.5;
+const iscool=true;
+const x=null;
+const y = undefined;
+let z;
+console.log(typeof name);        //output  string
+console.log(typeof age);                   number
+console.log(typeof rating);                number
+console.log(typeof iscool);                boolean
+console.log(typeof x);                     object
+console.log(typeof y);                     undefined
+console.log(typeof z);                     undefined*/
+
+
+
+//concatenation
+/*const name ='Agalya';
+const age= 20; 
+console.log('my name is '+ name +' and i am '+age);                //output: my name is agalya and i am 20
+//template string                                    
+console.log(`my name is ${name} and i am ${age}`);*/               //output: my name is agalya and i am 20
+
+
+
+
+
+
+//Strings
+/*const s='Hello World!'
+console.log(s.length);          output: 12
+console.log(s.toUpperCase());          HELLO WORLD!
+console.log(s.toLowerCase());          hello world
+console.log(s.substring(0,5));         Hello
+console.log(s.split(''));              'H' 'e' 'l' 'l' 'o' 'w' 'o' 'r' 'l' 'd' */
+
+
+
+//arrays
+/*const number=new Array(1,2,3,4,5);
+console.log(number);*/
+
+/*const fruits =['apples','oranges','pears'];
+console.log(fruits);
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
+fruits[3]='grapes';
+fruits.push('mangoes');
+fruits.unshift('strawberries');
+fruits.pop();
+console.log(Array.isArray(fruits));
+console.log(Array.isArray('hello'));
+console.log(fruits);*/
+
+
+
+//object key value pair
+/*const person = {
+    firstname : 'Agalya',
+    lastname : 'Palanisamy',
+    age : 20,
+    hobbies :['music','movies','books'],
+    address:{
+        street:'70/2 sakthimaari ammankovil street',
+        city:'Erode',
+        state:'Tamilnadu'
+    }
+}
+person.email='agalyapalanisamy001@gmail.com';
+console.log(person);
+console.log(person.firstname,person.lastname);
+console.log(person.hobbies[2]);
+console.log(person.address.city);
+
+const  {firstname,lastname,address:{city}}=person;
+console.log(firstname);
+console.log(lastname);
+console.log(city);*/
+
+
+
+
+//arrays with objects
